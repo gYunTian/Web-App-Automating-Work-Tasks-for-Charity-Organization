7191bce0de216b2199e8fc88b9ccccb91925ac2d
Sean Jin <seanjin.lee.2018@sis.smu.edu.sg>
2020-10-30 01:32:44 +0800

    add toggle for show basket

--- client2/components/foodbasket/Basket.js
+++ client2/components/foodbasket/Basket.js
@@ -1,24 +1,52 @@
-import { Component } from "react";
+import React, { Component } from "react";
+import FoodBasketButton from "./Buttons/FoodBasketButton.js";
 
 class Basket extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            data : props.data
-        }
+            data : props.data,
+            showBasket : false,
+            basketItems: null,
+            onToggleFoodItem: false
+        };
+
+        this.expandBasket = this.expandBasket.bind(this)
     }
 
+    expandBasket(id, foodItems) {
+        const toggled = this.state.showBasket ? false : true;
+        this.setState({showBasket: toggled,
+        basketItems: foodItems});
+    }
+
+    toggleAddRemove() {
+        const toggled = this.state.onToggleFoodItem ? false : true;
+        this.setState({onToggleFoodItem: toggled});
+    }
     render() {
+        const {data, showBasket, basketItems} = this.state;
         return (
             <div>
-            {this.state.data.map((item, index) => 
-            <div key = {index + new Date().getTime()}>
+                {data.map((item) => 
+            <div key = {item.BasketID}>
                 <span>
-                    {item.name}
+                    <FoodBasketButton onClick={() => this.expandBasket(item.BasketID, item.stocks)}>{item.name}</FoodBasketButton>
                 </span>
-                <span>{item.category}</span>
             </div>
             )}
+            <div> 
+                {showBasket && basketItems.map((item) => 
+                <div key={item.stock_stockID}>
+                    <span>
+                        {item.stock_stockID}
+                    </span>
+                    <span>
+                        {item.quantity}
+                    </span>
+                </div>
+                )}
+            </div>
         </div>
         )
     }
