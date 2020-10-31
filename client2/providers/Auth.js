b2fe01bc8d66d97473f721a975b621555eb4ca82
Sean Jin <seanjin.lee.2018@sis.smu.edu.sg>
2020-10-31 12:57:36 +0800

    update

--- client2/providers/Auth.js
+++ client2/providers/Auth.js
@@ -1,77 +1,76 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
 // set context
 const AuthContext = React.createContext({
-  name: null,
-  role: null,
-  isAuthenticated: false,
-  isLoading: true,
-  setAuthenticated: () => {},
+	name: null,
+	role: null,
+	isAuthenticated: false,
+	isLoading: true,
+	setAuthenticated: () => {},
 });
 
 // wraps around children component and add context
 export const AuthProvider = ({ children }) => {
-  const [name, setName] = React.useState("Test");
-  const [role, setRole] = React.useState("Test");
-  const [isAuthenticated, setAuthenticated] = React.useState(false);
-  const [isLoading, setLoading] = React.useState(true);
-  
-  // verify cookie against server
-  useEffect(() => {
-    const initializeAuth = async () => {
-      if (isAuthenticated) {
-        console.log("already authenticated");
-        return;
-      }
+	const [name, setName] = React.useState('Test');
+	const [role, setRole] = React.useState('Test');
+	const [isAuthenticated, setAuthenticated] = React.useState(false);
+	const [isLoading, setLoading] = React.useState(true);
 
-      try {
-        const response = await fetch("http://localhost:5000/api/auth/verify");
-        if (response === 200) {
-          console.log("server okay");
-          const data = await response.json();
-          setAuthenticated(true);
-          setLoading(false);
-          setName(data.name);
-          setRole(data.role);
+	// verify cookie against server
+	useEffect(() => {
+		const initializeAuth = async () => {
+			if (isAuthenticated) {
+				console.log('already authenticated');
+				return;
+			}
 
-        } else {
-          console.log("not authenticated");
-          setLoading(false);
-        }
-      } catch (error) {
-        console.log("server might be down");
-        setLoading(false);
-      }
-    };
-    initializeAuth();
-  }, []);
-  
-  return (
-    <AuthContext.Provider value={{ isAuthenticated, isLoading, name, role }}>
-      {children}
-    </AuthContext.Provider>
-  );
+			try {
+				const response = await fetch('http://localhost:5000/api/auth/verify');
+				if (response === 200) {
+					console.log('server okay');
+					const data = await response.json();
+					setAuthenticated(true);
+					setLoading(false);
+					setName(data.name);
+					setRole(data.role);
+				} else {
+					console.log('not authenticated');
+					setLoading(false);
+				}
+			} catch (error) {
+				console.log('server might be down');
+				setLoading(false);
+			}
+		};
+		initializeAuth();
+	}, []);
+
+	return (
+		<AuthContext.Provider value={{ isAuthenticated, isLoading, name, role }}>
+			{children}
+		</AuthContext.Provider>
+	);
 };
 
 export function useAuth() {
-  const context = React.useContext(AuthContext);
-  if (context === undefined) {
-    throw new Error("missing AuthProvider");
-  }
-  return context;
+	const context = React.useContext(AuthContext);
+	if (context === undefined) {
+		throw new Error('missing AuthProvider');
+	}
+	return context;
 }
 
 export function useIsAuthenticated() {
-  const context = useAuth();
-  return context.isAuthenticated;
+	const context = useAuth();
+	return context.isAuthenticated;
 }
 
 export function getName() {
-  const context = useAuth();
-  return context.name;
+	const context = useAuth();
+	return context.name;
 }
 
 export function getRole() {
-  const context = useAuth();
-  return context.role;
+	const context = useAuth();
+	return context.role;
 }
