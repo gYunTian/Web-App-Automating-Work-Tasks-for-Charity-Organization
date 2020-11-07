16763c6cfe232d6d7021feaee322f1e04eb03a17
gYunTian <54625060+gYunTian@users.noreply.github.com>
2020-11-07 19:16:55 +0800

    fix

---
+++ client/components/schedule/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = () =>{
+    return(
+        <div className="header my-4">
+            <h1 className="text-black font-bold">Planned Schedule</h1>
+        </div>
+    )
+}
+
+export default Title
